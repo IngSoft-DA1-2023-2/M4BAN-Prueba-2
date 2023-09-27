53ccdd3d0b4aa6885934d59a5abd6a3077283ccb
Milagros Ferrer <mf270160@fi365.orto.edu.uy>
2023-09-27 08:40:11 -0300

    [RED] Tile with negative area FAILS

--- BusinessLogic.Test/TileTest.cs
+++ BusinessLogic.Test/TileTest.cs
@@ -16,4 +16,18 @@ public class TileTest
         Assert.AreEqual(Material.Cement, tile.material);
         Assert.AreEqual(1, tile.area);
     }
+    
+    [TestMethod]
+    public void TileWithNegativeAreaShouldFailTest()
+    {
+        var tile = new Tile()
+        {
+            shape = Shape.Square,
+            material = Material.Cement,
+            area = -1,
+        };
+        Assert.AreEqual(Shape.Square, tile.shape);
+        Assert.AreEqual(Material.Cement, tile.material);
+        Assert.AreEqual(-1, tile.area);
+    }
 }
