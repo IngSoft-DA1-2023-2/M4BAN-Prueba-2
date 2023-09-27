05ff23931896cbc9c9450d00e9054bbb5fca1135
Milagros Ferrer <mf270160@fi365.orto.edu.uy>
2023-09-27 08:39:33 -0300

    [GREEN] New Tile Test

--- BusinessLogic.Test/TileTest.cs
+++ BusinessLogic.Test/TileTest.cs
@@ -9,10 +9,11 @@ public class TileTest
         var tile = new Tile()
         {
             shape = Shape.Square,
+            material = Material.Cement,
+            area = 1,
         };
         Assert.AreEqual(Shape.Square, tile.shape);
+        Assert.AreEqual(Material.Cement, tile.material);
+        Assert.AreEqual(1, tile.area);
     }
 }
-
-
-
--- BusinessLogic/Tile.cs
+++ BusinessLogic/Tile.cs
@@ -1,6 +1,20 @@
-namespace BusinessLogic.Test;
+namespace BusinessLogic;
 
 public class Tile
 {
     public Shape shape { get; set; }
+    public Material material { get; set; }
+    public double area { get; set; }
+}
+
+public enum Material
+{
+    Cement,
+    Brick
+}
+
+public enum Shape
+{
+    Square,
+    Circle
 }
