3e2d99945a1a94f5b1030e4d2abf0d432a957de8
Milagros Ferrer <mf270160@fi365.orto.edu.uy>
2023-09-27 08:56:37 -0300

    [REFACTOR] Tile with negative area FAILS

--- BusinessLogic.Test/TileTest.cs
+++ BusinessLogic.Test/TileTest.cs
@@ -10,24 +10,17 @@ public class TileTest
         {
             shape = Shape.Square,
             material = Material.Cement,
-            area = 1,
+            Area = 1,
         };
         Assert.AreEqual(Shape.Square, tile.shape);
         Assert.AreEqual(Material.Cement, tile.material);
-        Assert.AreEqual(1, tile.area);
+        Assert.AreEqual(1, tile.Area);
     }
     
     [TestMethod]
     public void TileWithNegativeAreaShouldFailTest()
     {
-        var tile = new Tile()
-        {
-            shape = Shape.Square,
-            material = Material.Cement,
-            area = -1,
-        };
-        Assert.AreEqual(Shape.Square, tile.shape);
-        Assert.AreEqual(Material.Cement, tile.material);
-        Assert.AreEqual(-1, tile.area);
+        var exeption = Assert.ThrowsException<ArgumentException>(() => new Tile(Shape.Square, Material.Cement, -1));
+        Assert.AreEqual("Area cannot be negative", exeption.Message);
     }
 }
--- BusinessLogic/Tile.cs
+++ BusinessLogic/Tile.cs
@@ -2,9 +2,33 @@ namespace BusinessLogic;
 
 public class Tile
 {
+    private double _area;
+
+    public Tile()
+    {
+    }
+    
+    public Tile (Shape shape, Material material, double area)
+    {
+        this.shape = shape;
+        this.material = material;
+        this.Area = area;
+    }
+
     public Shape shape { get; set; }
     public Material material { get; set; }
-    public double area { get; set; }
+    public double Area 
+    { 
+        get => _area;
+        set
+        {
+            if (value < 0)
+            {
+                throw new ArgumentException("Area cannot be negative");
+            }
+            _area = value;
+        }
+    }
 }
 
 public enum Material
