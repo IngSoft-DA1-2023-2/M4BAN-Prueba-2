cab5e4eb4e5699a7e2bbad582f0f4d78cddf18a5
Emilia <emiliamiles02@gmail.com>
2023-09-27 08:40:06 -0300

    [RED] CalculateTotalCost_Valid test

---
+++ BusinessLogic.Test/CostCalculatorTest.cs
@@ -0,0 +1,34 @@
+namespace BusinessLogic.Test;
+
+[TestClass]
+public class CostCalculatorTest
+{
+    /*Una empresa de baldosas quiere programar un sistema para calcular los costos de sus trabajos.
+     Las baldosas pueden ser cuadradas o circulares. Además, pueden ser de uno de los siguientes 
+     materiales: Cemento ($5) o Ladrillo ($10). El precio de cada baldosa se calcula como área de la 
+     baldosa * precio del metro cuadrado * precio del material.
+     
+     Modelar el problema usando TDD. El modelado debe tener una clase CostCalculator con un único 
+     método el cual recibe dos parámetros: una lista de baldosas y el precio por metro cuadrado, y 
+     devuelve el costo total.
+     */
+    [TestMethod]
+    public void CalculateTotalCost_Valid()
+    {
+        //Arrange
+        List<Valdosa> valdosas = new List<Valdosa>
+        {
+            new Valdosa(TipoValdosa.Cuadrado, Material.Cemento, 4),
+            new Valdosa(TipoValdosa.Circulo, Material.Ladrillo, 2)
+        };
+        int precioMetroCuad = 5;
+        
+        //Act
+        CostCalculator calc = new CostCalculator();
+        int totalCost = calc.Calculator(valdosas, precioMetroCuad);
+
+        //Assert
+        Assert.AreEqual(70, totalCost);
+
+    }
+}
