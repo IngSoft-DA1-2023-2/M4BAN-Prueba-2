e513dfeb75bf126b2cfe06be487201eba4f7e02b
Felipe <lussichfelipe173@gmail.com>
2023-09-27 08:26:27 -0300

    [RED] Create New Tile Test

---
+++ BusinessLogic.Test/TileTests.cs
@@ -0,0 +1,12 @@
+namespace BusinessLogic.Test;
+
+[TestClass]
+public class TileTests
+{
+    [TestMethod]
+    public void CreateNewTileTest()
+    {
+        var tile = new Tile();
+        Assert.IsNotNull(tile);
+    }
+}
