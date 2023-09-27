06dec1dc603135de61445d326dce55044d3175c7
Ignacio Vera <IV265444@fi365.ort.edu.uy>
2023-09-27 08:35:20 -0300

    [RED] Creo test Clacular_Costo_Total

--- BusinessLogic.Test/CostCalculatorTest.cs
+++ BusinessLogic.Test/CostCalculatorTest.cs
@@ -14,4 +14,25 @@ public class CostCalculatorTest
         // Assert
         Assert.IsNotNull(costCalculator);
     }
+
+    [TestMethod]
+    public void Calcular_Costo_Total()
+    {
+        // Arrange
+        var costCalculator = new CostCalculator();
+        var baldosa1 = new Baldosa("Cuadrado", 2, "Cemento");
+        var baldosa2 = new Baldosa("Circulo", 3, "Cemento");
+        var listaBaldosas = new List<Baldosa>
+        {
+            baldosa1,
+            baldosa2
+        };
+        var precioMetroCuadrado = 5;
+        var expected = 100 + (Math.PI * 3 * 3 * 5);
+        // Act
+        var actual = costCalculator.CalculateTotalCost(listaBaldosas, precioMetroCuadrado);
+
+        // Assert
+        Assert.AreEqual(expected, actual);
+    }
 }
