1bb189ab1e6b2938924d75362dd54d2d70dfdb39
IsabellaConti <101658396+IsabellaConti@users.noreply.github.com>
2023-09-27 09:23:17 -0300

    Tryout part

--- TryOut/Program.cs
+++ TryOut/Program.cs
@@ -1,3 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using BusinessLogic;
 
-Console.WriteLine("Hello, World!");
+class Program
+{
+    static void Main(string[] args)
+    {
+        Material cement = new Material()
+        {
+            Name = "Cement",
+            Cost = 5
+        };
+        ITile tile = new SquareTile()
+        {
+            Material = cement,
+            SideLength = 2
+        };
+
+        Material ladrillo = new Material()
+        {
+            Name = "ladrillo",
+            Cost = 5
+        };
+        ITile circleTile = new CircularTile()
+        {
+            Material = ladrillo,
+            Radius = 3
+        };
+        List<ITile> tiles = new List<ITile>()
+        {
+            tile,
+            tile,
+            tile,
+            circleTile,
+        };
+        CostCalculator calculator = new CostCalculator();
+        Console.Write(calculator.CalculateCost(tiles,3));
+    }
+}
