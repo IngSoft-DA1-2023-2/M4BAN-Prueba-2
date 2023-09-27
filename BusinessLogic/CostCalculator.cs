e74529c4dc5bcfcd62645ba794f979966d3adb50
belu <beludb1408@gmail.com>
2023-09-27 09:29:13 -0300

    [GREEN] CalculateTotalCost_ForOneSquareCementTile_ShouldReturnCorrectCost

--- BusinessLogic/CostCalculator.cs
+++ BusinessLogic/CostCalculator.cs
@@ -5,7 +5,10 @@ public class CostCalculator
     public static double CalculateTotalCost(List<Tile> tiles, double pricePerSqm)
     {
         double totalCost = 0;
-        
+        foreach (Tile tile in tiles)
+        {
+            totalCost += tile.CalculateCost(pricePerSqm);
+        }
         return totalCost;
     }
     
