df0186908d71aff1c9243f8b2374c307b20f5768
Martina Roll <marturoll11@gmail.com>
2023-09-27 08:39:49 -0300

    [RED] nuevaBaldosaInicilizacionAtributos

--- BusinessLogic.Test/UnitTest1.cs
+++ BusinessLogic.Test/UnitTest1.cs
@@ -10,4 +10,15 @@ public class Baldosa
         Assert.IsNotNull(baldosa);
     } 
     
+    [TestMethod]
+    
+    public void nuevaBaldosaInicilizacionAtributos()
+    {
+        string tipo = "circular";
+        string material = "ladrillo";
+        Baldosa baldosa = new Baldosa(tipo, material);
+        
+        Assert.AreEqual(tipo, baldosa.Tipo);
+    }
+    
 }
