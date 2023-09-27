e7344faa494ab13cdd343c2b501c4cdaff9baed2
belu <beludb1408@gmail.com>
2023-09-27 09:24:56 -0300

    [RED] CalculateTotalCost_ForEmptyTileList_ShouldReturnZero

--- BusinessLogic/CostCalculator.cs
+++ BusinessLogic/CostCalculator.cs
@@ -2,6 +2,10 @@ namespace BusinessLogic;
 
 public class CostCalculator
 {
-   
+    public static double CalculateTotalCost(List<Tile> tiles, double pricePerSqm)
+    {
+       
+        
+    }
     
 }
