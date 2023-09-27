5b28fcfaca347e67983d7fd6aac94e8dd3a87b8f
belu <beludb1408@gmail.com>
2023-09-27 09:28:39 -0300

    [RED] CalculateTotalCost_ForOneSquareCementTile_ShouldReturnCorrectCost

--- BusinessLogic.Test/CostCalculatorTest.cs
+++ BusinessLogic.Test/CostCalculatorTest.cs
@@ -16,6 +16,23 @@ public class CostCalculatorTest
     // Assert
     Assert.AreEqual(0, totalCost);
   }
+  
+  [TestMethod]
+  public void CalculateTotalCost_ForOneSquareCementTile_ShouldReturnCorrectCost()
+  {
+    // Arrange
+    List<Tile> tiles = new List<Tile>
+    {
+      new Tile("cuadrada", 2, "cemento")
+    };
+    double pricePerSqm = 5;
+
+    // Act
+    double totalCost = CostCalculator.CalculateTotalCost(tiles, pricePerSqm);
+
+    // Assert
+    Assert.AreEqual(2 * 2 * 5 * 5, totalCost);
+  }
 
 
 }
--- BusinessLogic/CostCalculator.cs
+++ BusinessLogic/CostCalculator.cs
@@ -4,15 +4,9 @@ public class CostCalculator
 {
     public static double CalculateTotalCost(List<Tile> tiles, double pricePerSqm)
     {
-        if (tiles.Count == 0)
-        {
-            return 0;
-        }
-        else
-        {
-            return 0; 
-        }
+        double totalCost = 0;
         
+        return totalCost;
     }
     
 }
