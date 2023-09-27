7171e7cdd0e8e2bf0461f3a6fec1536974f91cc9
josefina <josefi31@outlook.com>
2023-09-27 09:04:44 -0300

    [REFACTOR] modificacion nombre TestCalculator

--- BusinessLogic.Test/UnitTest1.cs
+++ BusinessLogic.Test/UnitTest1.cs
@@ -4,8 +4,9 @@ namespace BusinessLogic.Test;
 public class UnitTest1
 {
     [TestMethod]
-    public void TestMethod1()
+    public void TestCalculator()
     {
+        //arrange
         var calculator = new CostCalculator();
         var cost = calculator.CalculateCost(10, 10, 800);
         
--- BusinessLogic/CostCalculator.cs
+++ BusinessLogic/CostCalculator.cs
@@ -2,11 +2,9 @@ namespace BusinessLogic;
 
 public class CostCalculator
 {
-    //Una empresa de baldosas quiere programar un sistema para calcular los costos de sus trabajos. Las baldosas pueden ser cuadradas o circulares. Además, pueden ser de uno de los siguientes materiales: Cemento ($5) o Ladrillo ($10). El precio de cada baldosa se calcula como área de la baldosa * precio del metro cuadrado * precio del material.
-    
-    
-    public int CalculateCost(int length, int width, int costPerSquareMeter)
+    public int CalculateCost(int largo, int ancho, int costoPorMetroCuadrado)
     {
-        return length * width * costPerSquareMeter;
+        return largo * ancho * costoPorMetroCuadrado;
     }
+    
 }
