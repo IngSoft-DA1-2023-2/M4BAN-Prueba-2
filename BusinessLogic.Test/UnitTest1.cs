8301e5d8f28ee830cb5375e39760acd007752616
Ignacio Vera <IV265444@fi365.ort.edu.uy>
2023-09-27 08:18:49 -0300

    [RED] Creo test inicial Crear_Objeto_CostCalculator en clase CostCalculator.test

--- BusinessLogic.Test/UnitTest1.cs
+++ BusinessLogic.Test/UnitTest1.cs
@@ -1,10 +1,17 @@
 namespace BusinessLogic.Test;
 
 [TestClass]
-public class UnitTest1
+public class CostCalculatorTest
 {
     [TestMethod]
-    public void TestMethod1()
+    public void Crear_Objeto_CostCalculator()
     {
+        // Arrange
+        var costCalculator = new CostCalculator();
+
+        // Act
+
+        // Assert
+        Assert.IsNotNull(costCalculator);
     }
 }
